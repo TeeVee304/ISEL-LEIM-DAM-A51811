b5d444fbe9f12b77451fb36c7bda1b7fda6b996b
Bruno Pereira <brunonmoura@gmail.com>
2026-04-03 12:08:16 +0100

    Pipeline Class: execute and describe Methods Added

--- tutorial-2/kotlin/src/main/kotlin/pipeline.kt
+++ tutorial-2/kotlin/src/main/kotlin/pipeline.kt
@@ -5,4 +5,18 @@ class Pipeline {
     fun addStage(name: String, transform: (List<String>) -> List<String>) {
         stages.add(Pair(name, transform))
     }
+
+    // .fold(): pega no input inicial e passa o resultado de cada etapa para a seguinte
+    fun execute(input: List<String>): List<String> {
+        return stages.fold(input) { current, stage ->
+            stage.second(current) // stage.second = segundo elemento do par (Pair)
+        }
+    }
+
+    fun describe() {
+        println("Pipeline stages :")
+        stages.forEachIndexed { index, stage ->
+            println("${index + 1}. ${stage.first}")
+        }
+    }
 }
